eec223150bec4ba3060fd82d65d4288a7ab11d62
Tyranikwerewolf <83700003+Gaeram@users.noreply.github.com>
2022-07-06 17:04:26 +0200

    Adding CategoryConroller with the same fonctionnalities used in ArticleController

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ArticleCategory;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route("/insert-category", name: "insert-category")]
+    public function insertCategory(EntityManagerInterface $entityManager){
+
+        // Appel de l'instance ArticleCategory dans une variable
+        $category = new ArticleCategory();
+        // Utilisation des setters afin de définir nos données
+        $category->setTitle("Animaux");
+        $category->setColor("Blue");
+        $category->setDescription("Regroupe les articles sur le theme animalier");
+        $category->setIsPublished(true);
+        // Utilisation de ces deux fonctions appartenant à EntityManagerInterface
+        // Pour les enregistrer dans la table dédié dans la bdd
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        dump($category);
+        die();
+
+    }
+
+}
