a9b401140bc86e6d3f249aeb85b4085fe5ddc56a
Tyranikwerewolf <83700003+Gaeram@users.noreply.github.com>
2022-07-06 10:38:19 +0200

    Adding some comments in entity

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,9 +5,14 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 
+
+//Création d'une nouvelle entité grâce à la fonctionnalité ORM de Doctrine
 /**
  * @ORM\Entity()
  */
+
+// Création d'une nouvelle classe au nom de la page
+// Pour y noter nos ORM mappés qui vont permettre la création de tables
 class Article
 {
     /**
