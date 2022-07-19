5b6483bb7daee74339c59fa7abb906d5441490bb
Tyranikwerewolf <83700003+Gaeram@users.noreply.github.com>
2022-07-19 10:42:33 +0200

    adding image upload possibility in database

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -59,6 +59,29 @@ class Article
     }
 
     /**
+     * @ORM\Column(type="string")
+     */
+    private $image;
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image): void
+    {
+        $this->image = $image;
+    }
+
+
+
+    /**
      * @return mixed
      */
     public function getCategory()
