bcd8ae9543b512f593d818a3aee82b4559bfa820
Tyranikwerewolf <83700003+Gaeram@users.noreply.github.com>
2022-07-22 09:45:31 +0200

    dark mode with local storage function to save it in navigator to use it in all pages

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,23 @@
-const nightToggle = document.querySelector('.js-night-toggle')
+
+
 const body = document.querySelector('.js-body');
+// je regarde l'activation du dark mode dans le local storage
+// si oui, j'active la propriété css visée
+if(localStorage.getItem('nightActivated') === 'true'){
+    body.classList.add('night-activated')
+}
+const nightToggle = document.querySelector('.js-night-toggle')
 
 nightToggle.addEventListener('click',function () {
-
-
-
+    // si le dark mode est activé, je le désactive
     if (body.classList.contains('night-activated')) {
         body.classList.remove('night-activated');
+        // je le retire du local storage
+        localStorage.removeItem('nightActivated');
     } else {
+        // s'il n'est pas activé, je l'active
         body.classList.add('night-activated');
+        // je l'enregiste dans le local storage afin de le laisser activer sur toutes les pages
+        localStorage.setItem('nightActivated', 'true');
     }
 });
